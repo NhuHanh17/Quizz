b6a4f18f11ea0ab1be13bfe29681ce647a0f100e
NhuHanh17 <hanhxuk@gmail.com>
2025-07-23 15:10:24 +0700

    Delete QuizzApp directory

--- QuizzApp/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.han.quizzapp {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.base;
-
-    opens com.han.quizzapp to javafx.fxml;
-    exports com.han.quizzapp;
-}
